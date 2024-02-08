d2da5396a85f810a86be5fcbddaf509e735b5ffa
InsideTheSimulation <insidethesimulation@pm.me>
2024-02-08 11:56:34 -0500

    adds some test frame metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,34 +1,57 @@
 const head = {
-  title: '💪 RatioGang 📈 - The ETH / BTC Ratio Tracker',
+  title: "💪 RatioGang 📈 - The ETH / BTC Ratio Tracker",
   meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { hid: 'description', name: 'description', content: 'Ratio Gang assemble! Monitor the progress of Ethereum (ETH) in overtaking Bitcoin (BTC) by market cap.' },
-    { hid: 'twitter:image', name: 'twitter:image', content: 'https://ratiogang.com/og.png' }
-  ]
-}
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    {
+      hid: "description",
+      name: "description",
+      content:
+        "Ratio Gang assemble! Monitor the progress of Ethereum (ETH) in overtaking Bitcoin (BTC) by market cap.",
+    },
+    {
+      hid: "twitter:image",
+      name: "twitter:image",
+      content: "https://ratiogang.com/og.png",
+    },
+    { hid: "fc:frame", name: "fc:frame", content: "vNext" },
+    {
+      hid: "fc:frame:image",
+      name: "fc:frame:image",
+      content: "https://ratiogang.com/og.png",
+    },
+    {
+      hid: "fc:frame:button:1",
+      name: "fc:frame:button:1",
+      content: "Check the Ratio",
+    },
+    {
+      hid: "fc:frame:post_url",
+      name: "fc:frame:post_url",
+      content: "https://ratiogang.com/frame",
+    },
+  ],
+};
 
 export default {
-  target: 'static',
+  target: "static",
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head,
 
   env: {
-    head
+    head,
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '~/assets/scss/main.scss'
-  ],
+  css: ["~/assets/scss/main.scss"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '~/plugins/vue-select',
-    { src: '~/plugins/observe', mode: 'client' },
-    { src: '~/plugins/touch', mode: 'client' },
-    { src: '~/plugins/vue-confetti', mode: 'client' }
+    "~/plugins/vue-select",
+    { src: "~/plugins/observe", mode: "client" },
+    { src: "~/plugins/touch", mode: "client" },
+    { src: "~/plugins/vue-confetti", mode: "client" },
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -37,49 +60,49 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/color-mode'
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    'cookie-universal-nuxt',
-    '@nuxtjs/google-gtag',
-    'cookie-universal-nuxt'
+    "@nuxtjs/pwa",
+    "cookie-universal-nuxt",
+    "@nuxtjs/google-gtag",
+    "cookie-universal-nuxt",
   ],
 
   colorMode: {
-    classSuffix: '',
-    preference: 'dark',
-    fallback: 'dark'
+    classSuffix: "",
+    preference: "dark",
+    fallback: "dark",
   },
 
   // PWA config
   pwa: {
     meta: {
-      ogSiteName: '💪 RatioGang 📈 - The ETH / BTC Ratio Tracker',
-      ogTitle: '💪 RatioGang 📈 - The ETH / BTC Ratio Tracker',
-      ogDescription: 'Ratio Gang assemble! Monitor the progress of Ethereum (ETH) in overtaking Bitcoin (BTC) by market cap.',
-      ogHost: 'https://ratiogang.com',
-      ogImage: '/og.png',
-      twitterCard: 'summary_large_image'
-    }
+      ogSiteName: "💪 RatioGang 📈 - The ETH / BTC Ratio Tracker",
+      ogTitle: "💪 RatioGang 📈 - The ETH / BTC Ratio Tracker",
+      ogDescription:
+        "Ratio Gang assemble! Monitor the progress of Ethereum (ETH) in overtaking Bitcoin (BTC) by market cap.",
+      ogHost: "https://ratiogang.com",
+      ogImage: "/og.png",
+      twitterCard: "summary_large_image",
+    },
   },
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
 
-  'google-gtag': {
-    id: 'G-VN8P1W31CL'
+  "google-gtag": {
+    id: "G-VN8P1W31CL",
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+  build: {},
+};
