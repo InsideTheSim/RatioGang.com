18c90be5ac502fa0733554551efe149238fcf8a8
InsideTheSimulation <insidethesimulation@pm.me>
2024-08-30 09:25:24 -0400

    feat: fix background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,15 @@
 <template>
-  <div class="layout-container flex flex-col bg-gradient-to-b from-blue-50 to-blue-50 dark:from-gray-800 dark:to-gray-800">
+  <div class="layout-container flex flex-col bg-blue-50 dark:bg-gray-800">
     <TheSiteHeader />
-    <div
-      v-show="loaded"
-      class="page-content"
-    >
+    <div v-show="loaded" class="page-content">
       <PriceStats />
       <Nuxt />
       <TheSiteFooter />
     </div>
-    <div
-      class="mb-auto mt-32 text-center"
-      v-show="!loaded"
-    >
-      <h2 class="text-2xl md:text-3xl text-gray-800 dark:text-gray-300">loading...</h2>
+    <div class="mb-auto mt-32 text-center" v-show="!loaded">
+      <h2 class="text-2xl md:text-3xl text-gray-800 dark:text-gray-300">
+        loading...
+      </h2>
       <span class="text-4xl flipper">🐬</span>
     </div>
     <EasterEgg />
@@ -21,19 +17,19 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from 'vuex';
 
 export default {
   computed: {
     ...mapGetters({
       wsPriceFeed: 'system/webSocketPriceFeed',
-      fallbackPriceFeed: 'system/fallbackPriceFeed'
+      fallbackPriceFeed: 'system/fallbackPriceFeed',
     }),
-    loaded () {
-      return !!this.wsPriceFeed || !!this.fallbackPriceFeed
-    }
-  }
-}
+    loaded() {
+      return !!this.wsPriceFeed || !!this.fallbackPriceFeed;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -42,7 +38,7 @@ export default {
     transform: rotate(0deg);
   }
   to {
-    transform:  rotate(-360deg);
+    transform: rotate(-360deg);
   }
 }
 
